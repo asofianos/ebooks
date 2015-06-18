fd55b09d92e6cae16fcf54f53a8f2fd1543b6a1b
Alexandros <metaxata07@gmail.com>
2015-06-18 17:07:58 +0300

    adasdasasas

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,8 @@ router.get('/hello/:value', function(req, res, next) {
   res.render('index', { title: req.params.value });
 });
 
+router.get('/hello1/:value', function(req, res, next) {
+  res.render('index', { title: req.params.value });
+});
+
 module.exports = router;
